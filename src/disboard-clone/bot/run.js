2c46fbcdf162247c7011a299de8035cff5b49c6b
mattchew1010 <mattchewiscool@gmail.com>
2023-08-02 23:40:03 -0400

    server join and user presence change events

--- src/disboard-clone/bot/run.js
+++ src/disboard-clone/bot/run.js
@@ -1,30 +1,42 @@
+require('dotenv').config();
 const { Client, GatewayIntentBits, Events} = require('discord.js');
 const database = require('../database/database.js');
 const { data } = require('autoprefixer');
 const token = process.env.discord_bot_token //temp move to .env
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildPresences]});
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildMembers]});
 
 
 client.once(Events.ClientReady, () => {
-	client.user.setPresence({ activities: [{ name: 'around' }], status: 'online' });
+	
 });
 client.on(Events.GuildCreate, (guild) => {
    console.log(`Joined ${guild.name}`)
    //guild.members.cache.each(member => console.log(member.presence))
-   database.serverJoined(guild.id, guild.memberCount, guild.members.cache.filter(member => (member.presence != null && member.presence.status != "offline")).size)
+   database.createServer(guild.id, guild.memberCount, guild.members.cache.filter(member => (member.presence != null && member.presence.status != "offline")).size)
+   guild.members.fetch({withPresences: true})
+      .then(members => {
+         members.each(member => {
+            database.createUser(guild.id, member.id, member.presence ? member.presence.status : "offline")
+         })
+      })
+      .catch(() => console.warn("Failed to fetch members"));
 });
 
 client.on(Events.PresenceUpdate, (oldPresence, newPresence) => {
+   if (newPresence == null) return; //nothing we can do here
    if (oldPresence == null){
-
+      database.getUser(newPresence.user.id, newPresence.guild.id, (data) => {
+         if (data.length == 0){
+            database.createUser(newPresence.guild.id, newPresence.user.id, newPresence.status)
+         }else{
+            database.userPresenceChanged(newPresence.status, newPresence.guild.id, newPresence.user.id)
+         }
+      });
+      return; //end of gaurd clause so return
    }
    if (oldPresence.status != newPresence.status) {
-      if (newPresence.status == "online"){
-         database.userPresenceChanged(true, newPresence.guild.id, newPresence.user.id)
-      }else {
-         database.userPresenceChanged(false, newPresence.guild.id, newPresence.user.id)
-      }
+      database.userPresenceChanged(newPresence.status, newPresence.guild.id, newPresence.user.id)      
    }
    
 });
--- src/disboard-clone/database/database.js
+++ src/disboard-clone/database/database.js
@@ -11,16 +11,30 @@ con.connect(function(err) {
   console.log("Connected!");  
 });
 
-function userPresenceChanged(isOnline, guildId, userId){
-      con.query("UPDATE servers SET online_users = online_users + IF(?, 1, -1) WHERE server_id = ?", [isOnline, guildId], function (err, result, fields) {  
+function userPresenceChanged(status, guildId, userId){
+      con.query("UPDATE servers SET online_users = online_users + IF(?, 1, -1) WHERE server_id = ?", [(status == "online") ? true : false, guildId], function (err, result, fields) {  
          if (err) throw err;  
+      });
+      con.query("UPDATE users SET status = ? WHERE server_id = ? AND user_id = ?", [status, guildId, userId], function (err, result, fields) {
+          if (err) throw err;
       });
 }
 
+function getUser(userId, guildId, cb){
+    con.query("SELECT * FROM users WHERE server_id = ? AND user_id = ?", [guildId, userId], function (err, result, fields) {
+        if (err) throw err;
+        cb(fields)
+    });
+}
 
-function serverJoined(guildId, totalUsers, onlineUsers){
+function createServer(guildId, totalUsers, onlineUsers){
    con.query("INSERT INTO servers (server_id, total_users, online_users) VALUES (?, ?, ?)", [guildId, totalUsers, onlineUsers], function (err, result, fields) {
-   console.log(err);
    });
 }
-module.exports = {serverJoined, userPresenceChanged};
+function createUser(guildId, userId, status){
+   con.query("INSERT INTO users (server_id, user_id, status) VALUES (?, ?, ?)", [guildId, userId, status], function (err, result, fields) {
+   });
+}
+
+
+module.exports = {userPresenceChanged, createServer, createUser, getUser};
